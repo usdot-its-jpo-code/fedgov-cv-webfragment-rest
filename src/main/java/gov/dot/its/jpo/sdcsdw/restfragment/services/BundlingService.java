b893fddb944fb82c42a35cb81b7cbc3444d1e74a
Karen McShane <karen.e.mcshane@aero.org>
2019-02-06 17:25:26 -0500

    Start adding logic for bundle and distribution functions

--- src/main/java/gov/dot/its/jpo/sdcsdw/restfragment/services/BundlingService.java
+++ src/main/java/gov/dot/its/jpo/sdcsdw/restfragment/services/BundlingService.java
@@ -1,5 +1,13 @@
-package gov.usdot.restfragment.services;
+package gov.dot.its.jpo.sdcsdw.restfragment.services;
+
+import java.util.List;
+
+import gov.dot.its.jpo.sdcsdw.Models.AdvisorySituationBundle;
+import gov.dot.its.jpo.sdcsdw.Models.AdvisorySituationData;
+import gov.dot.its.jpo.sdcsdw.Models.AdvisorySituationDataDistribution;
+import gov.dot.its.jpo.sdcsdw.restfragment.model.Query;
 
 public interface BundlingService {
-
+	public AdvisorySituationBundle bundle(List<AdvisorySituationData> asd, Query query);
+	public AdvisorySituationDataDistribution distribution(List<AdvisorySituationData> asd, Query query);
 }
--- src/main/java/gov/dot/its/jpo/sdcsdw/restfragment/services/BundlingServiceImpl.java
+++ src/main/java/gov/dot/its/jpo/sdcsdw/restfragment/services/BundlingServiceImpl.java
@@ -1,5 +1,35 @@
-package gov.usdot.restfragment.services;
+package gov.dot.its.jpo.sdcsdw.restfragment.services;
+
+import java.util.List;
+import java.util.Random;
+
+import gov.dot.its.jpo.sdcsdw.Models.AdvisorySituationBundle;
+import gov.dot.its.jpo.sdcsdw.Models.AdvisorySituationData;
+import gov.dot.its.jpo.sdcsdw.Models.AdvisorySituationDataDistribution;
+import gov.dot.its.jpo.sdcsdw.Models.AsdRecords;
+import gov.dot.its.jpo.sdcsdw.restfragment.model.Query;
+
 
 public class BundlingServiceImpl implements BundlingService {
+
+	@Override
+	public AdvisorySituationBundle bundle(List<AdvisorySituationData> asd, Query query) {
+		AdvisorySituationBundle bundle = new AdvisorySituationBundle();
+		AsdRecords recs = new AsdRecords();
+		//recs.setAdvisoryBroadcast(MessageCreator.extractTimsAndGenerateBroadcasts(asd).toArray());
+			
+		bundle.setAsdRecords(recs);
+		bundle.setBundleId(String.format("%08X", new Random().nextInt()));
+		bundle.setBundleNumber("1");
+			
+		return bundle;
+	} 
+	
+	
+	
+	@Override
+	public AdvisorySituationDataDistribution distribution(List<AdvisorySituationData> asd, Query query) {
+		return null;
+	}
 
 }
