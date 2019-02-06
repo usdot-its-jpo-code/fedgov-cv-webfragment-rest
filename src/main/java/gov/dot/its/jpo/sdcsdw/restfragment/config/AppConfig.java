7ec1a10690fe6e72d2391fb410337ccda05744b6
Andrew M Melnick <andrew.m.melnick@aero.org>
2019-02-06 12:08:00 -0500

    Remove unnecessary components from AppConfig

--- src/main/java/gov/dot/its/jpo/sdcsdw/restfragment/config/AppConfig.java
+++ src/main/java/gov/dot/its/jpo/sdcsdw/restfragment/config/AppConfig.java
@@ -1,16 +1,10 @@
 package gov.dot.its.jpo.sdcsdw.restfragment.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
-import gov.dot.its.jpo.sdcsdw.restfragment.rest.QueryController;
-
 @Configuration
-@ComponentScan(basePackages = {"gov.dot.its.jpo.sdcsdw.restfragment", "gov.dot.its.jpo.sdcsdw.restfragment.rest", "gov.dot.its.jpo.sdcsdw.restfragment.services"})
+@ComponentScan(basePackages = {"gov.dot.its.jpo.sdcsdw.restfragment" })
 public class AppConfig {
-
-    @Bean
-    QueryController queryController() { return new QueryController(); }
-
+    
 }
