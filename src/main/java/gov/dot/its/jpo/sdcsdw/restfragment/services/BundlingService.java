cd3029e7120db5e915e9236f02118bacd3e709f5
Karen McShane <karen.e.mcshane@aero.org>
2019-02-08 17:07:02 -0500

    Methods now consume and produce lists of json objects

--- src/main/java/gov/dot/its/jpo/sdcsdw/restfragment/services/BundlingService.java
+++ src/main/java/gov/dot/its/jpo/sdcsdw/restfragment/services/BundlingService.java
@@ -1,13 +1,20 @@
 package gov.dot.its.jpo.sdcsdw.restfragment.services;
 
+import java.io.IOException;
 import java.util.List;
+
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
 
 import gov.dot.its.jpo.sdcsdw.Models.AdvisorySituationBundle;
 import gov.dot.its.jpo.sdcsdw.Models.AdvisorySituationData;
 import gov.dot.its.jpo.sdcsdw.Models.AdvisorySituationDataDistribution;
 import gov.dot.its.jpo.sdcsdw.restfragment.model.Query;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.ParseException;
 
 public interface BundlingService {
-	public List<AdvisorySituationBundle> createBundleList(List<AdvisorySituationData> asd, Query query);
-	public List<AdvisorySituationDataDistribution> createDistributionList(List<AdvisorySituationData> asd, Query query);
+	public List<JSONObject> bundleOrDistribute(List<JSONObject> jsonList, String packageType, String dialogID) throws JsonParseException, JsonMappingException, IOException, ParseException;
+	public List<AdvisorySituationBundle> createBundleList(List<AdvisorySituationData> asd, String dialogID);
+	public List<AdvisorySituationDataDistribution> createDistributionList(List<AdvisorySituationData> asd, String dialogID);
 }
--- src/main/java/gov/dot/its/jpo/sdcsdw/restfragment/services/BundlingServiceImpl.java
+++ src/main/java/gov/dot/its/jpo/sdcsdw/restfragment/services/BundlingServiceImpl.java
@@ -1,8 +1,16 @@
 package gov.dot.its.jpo.sdcsdw.restfragment.services;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+
+import org.codehaus.jackson.JsonFactory;
+import org.codehaus.jackson.JsonGenerationException;
+import org.codehaus.jackson.JsonParseException;
+import org.codehaus.jackson.JsonParser;
+import org.codehaus.jackson.map.JsonMappingException;
+import org.codehaus.jackson.map.ObjectMapper;
 
 import gov.dot.its.jpo.sdcsdw.Models.AdvisorySituationBundle;
 import gov.dot.its.jpo.sdcsdw.Models.AdvisorySituationData;
@@ -12,31 +20,64 @@ import gov.dot.its.jpo.sdcsdw.Models.AsdBundles;
 import gov.dot.its.jpo.sdcsdw.Models.DialogID;
 import gov.dot.its.jpo.sdcsdw.restfragment.model.Query;
 import gov.dot.its.jpo.sdcsdw.udpdialoghandler.service.MessageCreator;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 
 
 public class BundlingServiceImpl implements BundlingService {
 	@Override
-	public List<AdvisorySituationBundle> createBundleList(List<AdvisorySituationData> asd, Query query) {
-		List<AdvisorySituationDataDistribution> distributionList = createDistributionList(asd, query);
+	public List<JSONObject> bundleOrDistribute(List<JSONObject> jsonList, String packageType, String dialogId) throws JsonParseException, JsonMappingException, IOException, ParseException  {
+		ObjectMapper mapper = new ObjectMapper();
+		JSONParser parser = new JSONParser();
+		
+		List<AdvisorySituationData> asdList = new ArrayList<AdvisorySituationData>();
+		List<JSONObject> returnJsons = new ArrayList<JSONObject>();
+		
+		for (JSONObject json : jsonList) {
+			AdvisorySituationData asd = mapper.readValue(json.toString(), AdvisorySituationData.class);
+			asdList.add(asd);
+		}
+		
+		if (packageType.equals("bundle")) {
+			List<AdvisorySituationBundle> bundleList = createBundleList(asdList, dialogId);
+			
+			for (AdvisorySituationBundle bundle : bundleList) {		
+				JSONObject json = (JSONObject) parser.parse(mapper.writeValueAsString(bundle));
+				returnJsons.add(json);
+			}
+		} else {
+			
+		}
+		
+		return returnJsons;
+	}
+	
+	
+	@Override
+	public List<AdvisorySituationBundle> createBundleList(List<AdvisorySituationData> asd, String dialogId) {
+		List<AdvisorySituationDataDistribution> distributionList = createDistributionList(asd, dialogId);
 		List<AdvisorySituationBundle> bundleList = new ArrayList<AdvisorySituationBundle>();
 		
 		for (AdvisorySituationDataDistribution distribution : distributionList) {
 			AsdBundles bundles = distribution.getAsdBundles();
 			bundleList.addAll(Arrays.asList(bundles.getAdvisorySituationBundle()));
 		}
+
 		
 		return bundleList;
 	} 
 	
 	
 	@Override
-	public List<AdvisorySituationDataDistribution> createDistributionList(List<AdvisorySituationData> asd, Query query) {
+	public List<AdvisorySituationDataDistribution> createDistributionList(List<AdvisorySituationData> asd, String dialogId) {
 		DialogID dialogIDObject = new DialogID();
-		dialogIDObject.setAdvSitDatDist(query.getDialogId());
+		dialogIDObject.setAdvSitDatDist("");
 		AdvisorySituationDataDistributionList distributionListObject = MessageCreator.createAdvisorySituationDataDistributionList(asd, dialogIDObject, "00 00 00 00", "00 00 00 00");
+		
 		
 		return distributionListObject.getDistributionList();
 	}
-
+	
 }
