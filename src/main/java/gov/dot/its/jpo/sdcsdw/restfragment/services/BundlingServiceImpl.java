376380209e1902391ef40fe9f4081332b1719c62
Andrew M Melnick <andrew.m.melnick@aero.org>
2019-02-12 14:56:51 -0500

    Re-implement bundling to re-decode from XER

--- src/main/java/gov/dot/its/jpo/sdcsdw/restfragment/services/BundlingServiceImpl.java
+++ src/main/java/gov/dot/its/jpo/sdcsdw/restfragment/services/BundlingServiceImpl.java
@@ -6,8 +6,12 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import javax.xml.bind.JAXBException;
+
 import org.codehaus.jackson.JsonParseException;
 import org.codehaus.jackson.map.JsonMappingException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import gov.dot.its.jpo.sdcsdw.Models.AdvisorySituationBundle;
@@ -16,8 +20,15 @@ import gov.dot.its.jpo.sdcsdw.Models.AdvisorySituationDataDistribution;
 import gov.dot.its.jpo.sdcsdw.Models.AdvisorySituationDataDistributionList;
 import gov.dot.its.jpo.sdcsdw.Models.AsdBundles;
 import gov.dot.its.jpo.sdcsdw.Models.DialogID;
+import gov.dot.its.jpo.sdcsdw.asn1.perxercodec.Asn1Types;
+import gov.dot.its.jpo.sdcsdw.asn1.perxercodec.PerXerCodec;
+import gov.dot.its.jpo.sdcsdw.asn1.perxercodec.exception.CodecFailedException;
+import gov.dot.its.jpo.sdcsdw.asn1.perxercodec.exception.FormattingFailedException;
+import gov.dot.its.jpo.sdcsdw.asn1.perxercodec.exception.UnformattingFailedException;
+import gov.dot.its.jpo.sdcsdw.asn1.perxercodec.per.HexPerData;
+import gov.dot.its.jpo.sdcsdw.asn1.perxercodec.xer.RawXerData;
 import gov.dot.its.jpo.sdcsdw.udpdialoghandler.service.MessageCreator;
-
+import gov.dot.its.jpo.sdcsdw.xerjaxbcodec.XerJaxbCodec;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -27,35 +38,61 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Service
 public class BundlingServiceImpl implements BundlingService {
+    private final static Logger logger = LoggerFactory.getLogger(BundlingServiceImpl.class);
+    
 	@Override
 	public List<JsonNode> bundleOrDistribute(List<JsonNode> jsonList, String packageType, String dialogId) throws JsonParseException, JsonMappingException, IOException {
 		ObjectMapper mapper = new ObjectMapper();
 		
-		List<AdvisorySituationData> asdList = new ArrayList<AdvisorySituationData>();
-		List<JsonNode> returnJsons = new ArrayList<JsonNode>();
-		
-		for (JsonNode json : jsonList) {
-			//To be changed, currently implementing alternative to this is encoding service
-			AdvisorySituationData asd = mapper.treeToValue(json, AdvisorySituationData.class);
-			
-			asdList.add(asd);
-		}
-		
-		if (packageType.equals("bundle")) {
-			List<AdvisorySituationBundle> bundleList = createBundleList(asdList, dialogId);
-			
-			for (AdvisorySituationBundle bundle : bundleList) {
-				returnJsons.add(mapper.valueToTree(bundle));
-			}
-		} else {
-			List<AdvisorySituationDataDistribution> distributionList = createDistributionList(asdList, dialogId);
-			
-			for (AdvisorySituationDataDistribution distribution : distributionList) {
-				returnJsons.add(mapper.valueToTree(distribution));
-			}
+		List<JsonNode> returnJsons;
+		List<AdvisorySituationData> asdList;
+		switch(packageType) {
+		case "bundle":
+		    logger.info("Bundling");
+		    returnJsons = new ArrayList<>();
+		    asdList = decodeAsds(jsonList);
+		    List<AdvisorySituationBundle> bundleList = createBundleList(asdList, dialogId);
+		    for (AdvisorySituationBundle bundle : bundleList) {
+                returnJsons.add(mapper.valueToTree(bundle));
+            }
+		    break;
+		case "distribution":
+		    logger.info("Distributing");
+		    returnJsons = new ArrayList<>();
+		    asdList = decodeAsds(jsonList);
+		    List<AdvisorySituationDataDistribution> distributionList = createDistributionList(asdList, dialogId);
+		    for (AdvisorySituationDataDistribution distribution : distributionList) {
+		        JsonNode node = mapper.valueToTree(distribution);
+		        logger.info("Got node " + node.toString());
+                returnJsons.add(node);
+            }
+		    break;
+	    default:
+	        logger.info("No Packaging");
+	        returnJsons = jsonList;
+	        break;
 		}
 		
 		return returnJsons;
+	}
+	
+	private List<AdvisorySituationData> decodeAsds(List<JsonNode> jsonList) {
+	    List<AdvisorySituationData> asdList = new ArrayList<AdvisorySituationData>();
+	    
+	    for (JsonNode json : jsonList) {
+            //To be changed, currently implementing alternative to this is encoding service
+            //AdvisorySituationData asd = mapper.treeToValue(json, AdvisorySituationData.class);
+            String encodedMsgPerHex = json.get("encodedMsg").asText();
+            try {
+                String encodedMsgXerString = PerXerCodec.perToXer(Asn1Types.AdvisorySituationDataType, encodedMsgPerHex, HexPerData.unformatter, RawXerData.formatter);
+                AdvisorySituationData asd = (AdvisorySituationData) XerJaxbCodec.XerToJaxbPojo(encodedMsgXerString);
+                asdList.add(asd);
+            } catch(CodecFailedException | FormattingFailedException | UnformattingFailedException | JAXBException e) {
+                throw new RuntimeException(e);
+            }
+        }
+	    
+	    return asdList;
 	}
 	
 	
